995da35c8c33443a7058dbc1f8d31a5a94881ed1
d34d <clark@cyngn.com>
2015-10-28 14:11:01 -0700

    installd: Use -f (force overwrite) for aapt
    When re-creating resource apks for themes, aapt will fail if the
    apk already exists unless we use -f
    
    Change-Id: If2c3a634aa3ca061009aa6892530d643f8f67e9f
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- cmds/installd/commands.cpp
+++ cmds/installd/commands.cpp
@@ -1749,6 +1749,7 @@ static void run_aapt(const char *source_apk, const char *internal_path,
                               "-I", common_res_path,
                               "-r", resapk_str,
                               "-x", pkgId_str,
+                              "-f",
                               (char*)NULL);
     } else {
         execl(AAPT_BIN, AAPT_BIN, "package",
@@ -1759,6 +1760,7 @@ static void run_aapt(const char *source_apk, const char *internal_path,
                               "-I", FRAMEWORK_RES,
                               "-r", resapk_str,
                               "-x", pkgId_str,
+                              "-f",
                               (char*)NULL);
     }
     ALOGE("execl(%s) failed: %s\n", AAPT_BIN, strerror(errno));
